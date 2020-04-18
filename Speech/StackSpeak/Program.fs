e436d390761660cbf795a5f5385fa4d16a5e62cb
Chiel ten Brinke <ctenbrinke@gmail.com>
2020-04-18 20:17:22 +0200

    Make less vebose

--- Speech/StackSpeak/Program.fs
+++ Speech/StackSpeak/Program.fs
@@ -1,35 +1,40 @@
 ﻿open System
 open System.Speech.Recognition
     
-let sr_SpeechDetected sender (e:SpeechDetectedEventArgs)  =
-    printfn "Speech detected..."
+let sr_SpeechDetected verbose sender (e:SpeechDetectedEventArgs)  =
+    if verbose then printfn "Speech detected..."
 
-let sr_SpeechHypothesized sender (e:SpeechHypothesizedEventArgs)  =
-    //printfn "Speech hypothesized... (Confidence: %A) %s" e.Result.Confidence e.Result.Text
-    ()
+let sr_SpeechHypothesized verbose sender (e:SpeechHypothesizedEventArgs)  =
+    if verbose then printfn "Speech hypothesized... (Confidence: %A) %s" e.Result.Confidence e.Result.Text
 
-let sr_SpeechRecognized (interpreterState:Interpreter.State) sender (e:SpeechRecognizedEventArgs)  =
+let sr_SpeechRecognized verbose (interpreterState:Interpreter.State) sender (e:SpeechRecognizedEventArgs)  =
     let previousColor = Console.ForegroundColor
     if e.Result.Confidence > 0.9f then
         Console.ForegroundColor <- ConsoleColor.Green
-    else
+    else 
         Console.ForegroundColor <- ConsoleColor.Cyan
 
-    printfn "Speech recognized... (Confidence: %A) %s" e.Result.Confidence e.Result.Text
+    if verbose then
+        printfn "Speech recognized... (Confidence: %A) %s" e.Result.Confidence e.Result.Text
 
-    Console.ForegroundColor <- ConsoleColor.Red
     if e.Result.Confidence > 0.9f then
+        printfn "%s" e.Result.Text
+        Console.ForegroundColor <- ConsoleColor.Red
         match Parser.runParser e.Result.Text with
         | Some result -> Interpreter.processCommand interpreterState result
         | None -> ()
 
-    Console.ForegroundColor <- ConsoleColor.Yellow
-    printfn "Stack: %A" interpreterState.Stack
+        Console.ForegroundColor <- ConsoleColor.Yellow
+        printfn "Stack: %A" interpreterState.Stack
+
     Console.ForegroundColor <- previousColor
     ()
 
 [<EntryPoint>]
 let main argv =
+    let verbose = false
+    let writeGrammar = false
+
     let document = Grammar.createGrammarDocument
 
     // Create a Grammar object, initializing it with the root rule.
@@ -41,13 +46,13 @@ let main argv =
     //recognizer.EndSilenceTimeout <- TimeSpan.FromSeconds 1.0
     //recognizer.InitialSilenceTimeout <- TimeSpan.FromSeconds 3.0
     //recognizer.BabbleTimeout <- TimeSpan.FromSeconds 4.0
-    recognizer.EndSilenceTimeoutAmbiguous <- TimeSpan.FromSeconds 2.0
+    recognizer.EndSilenceTimeoutAmbiguous <- TimeSpan.FromSeconds 1.2
 
     let interpreterState = Interpreter.State()
 
-    recognizer.SpeechDetected.AddHandler(new EventHandler<SpeechDetectedEventArgs>(sr_SpeechDetected))
-    recognizer.SpeechHypothesized.AddHandler(new EventHandler<SpeechHypothesizedEventArgs>(sr_SpeechHypothesized))
-    recognizer.SpeechRecognized.AddHandler(new EventHandler<SpeechRecognizedEventArgs>(sr_SpeechRecognized interpreterState))
+    recognizer.SpeechDetected.AddHandler(new EventHandler<SpeechDetectedEventArgs>(sr_SpeechDetected verbose))
+    recognizer.SpeechHypothesized.AddHandler(new EventHandler<SpeechHypothesizedEventArgs>(sr_SpeechHypothesized verbose))
+    recognizer.SpeechRecognized.AddHandler(new EventHandler<SpeechRecognizedEventArgs>(sr_SpeechRecognized verbose interpreterState))
 
     // Load the Grammar object into the recognizer.
     recognizer.LoadGrammar(g);
@@ -59,9 +64,10 @@ let main argv =
     recognizer.RecognizeAsync(RecognizeMode.Multiple);  
 
     // Produce an XML file that contains the grammar.
-    //let writer = System.Xml.XmlWriter.Create("srgsDocument.xml")
-    //document.WriteSrgs(writer)
-    //writer.Close()
+    if writeGrammar then
+        let writer = System.Xml.XmlWriter.Create("srgsDocument.xml")
+        document.WriteSrgs(writer)
+        writer.Close()
 
     while (true) do
         Console.Read() |> ignore
