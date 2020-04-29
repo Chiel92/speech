b5cbd87c52e80fe7db1297e2c3372e71b40cd746
Chiel ten Brinke <ctenbrinke@gmail.com>
2020-04-29 23:39:54 +0200

    Make stack manipulation in-place

--- Speech/StackSpeak/Interpreter.fs
+++ Speech/StackSpeak/Interpreter.fs
@@ -20,38 +20,50 @@ type State() =
     member this.SetDefinition(name, definition) =
         _scope.[name] <- definition
 
-let rec processOperation (stack:Stack) (scope:Scope) (operation:Operation) =
-    try
+let rec processOperation (state:State) (scope:Scope) (operation:Operation) frameCount =
+    let stack = state.Stack
+    let newstack =
         match operation with
-        | Push x -> x :: stack
-        | Pull x -> stack.[x] :: (stack.[..(x-1)] @ stack.[(x+1)..])
-        | Add -> stack.[0] + stack.[1] :: List.skip 2 stack
-        | Subtract -> stack.[0] - stack.[1] :: List.skip 2 stack
-        | Multiply -> stack.[0] * stack.[1] :: List.skip 2 stack
-        | Duplicate -> stack.[0] :: stack
-        | Scratch -> List.skip 1 stack
-        | LessThan -> (if stack.[0] < stack.[1] then 1 else 0) :: List.skip 2 stack
+        | Push x -> Some <| x :: stack
+        | Pull x -> Some <| stack.[x] :: (stack.[..(x-1)] @ stack.[(x+1)..])
+        | Add -> Some <| stack.[0] + stack.[1] :: List.skip 2 stack
+        | Subtract -> Some <| stack.[0] - stack.[1] :: List.skip 2 stack
+        | Multiply -> Some <| stack.[0] * stack.[1] :: List.skip 2 stack
+        | Duplicate -> Some <| stack.[0] :: stack
+        | Scratch -> Some <| List.skip 1 stack
+        | LessThan -> Some <| (if stack.[0] < stack.[1] then 1 else 0) :: List.skip 2 stack
         | Maybe op ->
             if stack.[0] <> 0
-            then (processOperation (List.skip 1 stack) scope op)
-            else List.skip 1 stack
-        | Call d -> consumeOperations stack scope (snd scope.[d])
-    with
-    | e ->
-        printfn "Exception: %s" e.Message
-        stack
-and consumeOperations (stack:Stack) (scope:Scope) (operations:Operation list) =
+            then
+                state.SetStack(List.skip 1 stack)
+                (processOperation state scope op frameCount)
+                None
+            else Some <| List.skip 1 stack
+        | Call d ->
+            consumeOperations state scope (snd scope.[d]) (frameCount+1)
+            None
+    match newstack with
+    | Some s -> state.SetStack(s)
+    | None -> ()
+and consumeOperations state (scope:Scope) (operations:Operation list) frameCount =
+    if frameCount > 1000 then invalidOp "Recursion too deep"
     match operations with
-    | op::ops -> consumeOperations (processOperation stack scope op) scope ops
-    | [] -> stack
+    | [] -> ()
+    | op::ops -> 
+        processOperation state scope op frameCount
+        consumeOperations state scope ops frameCount
 
 let processDefinition (state:State) (definition:Definition) =
     state.SetDefinition(fst definition, definition)
     printfn "Saved definition %A" definition
 
 let processCommand (state:State) (command:Command) =
-    match command with
-    | Nop -> ()
-    | Undo -> state.SetStack(state.PreviousStack)
-    | Op o -> state.SetStack(processOperation state.Stack state.Scope o)
-    | Def d -> processDefinition state d
+    try
+        match command with
+        | Nop -> ()
+        | Undo -> state.SetStack(state.PreviousStack)
+        | Op o -> processOperation state state.Scope o 0
+        | Def d -> processDefinition state d
+    with
+    | e ->
+        printfn "Exception: %s" e.Message
