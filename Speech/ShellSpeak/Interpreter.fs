f907fed03c848dd9d8ed5422a9ca7d7f563eddbd
Chiel ten Brinke <ctenbrinke@gmail.com>
2020-10-14 10:54:51 +0200

    Working prototype for shellspeak

--- Speech/ShellSpeak/Interpreter.fs
+++ Speech/ShellSpeak/Interpreter.fs
@@ -1,12 +1,31 @@
 ﻿module Interpreter
 
-open System
+open System.Diagnostics
 open Language
 
-type State() = class end
+type State() =
+    class
+    end
 
-let processCommand (state:State) (command: Command) =
+let Bash (cmd: string): string =
+    let escapedArgs = cmd.Replace("\"", "\\\"")
+
+    let proc =
+        new Process(StartInfo =
+            new ProcessStartInfo(FileName = "cmd.exe",
+                                 Arguments = sprintf "/C \"%s\"" escapedArgs,
+                                 RedirectStandardOutput = true,
+                                 UseShellExecute = false,
+                                 CreateNoWindow = true))
+
+    proc.Start()
+    let result = proc.StandardOutput.ReadToEnd()
+    proc.WaitForExit()
+    result
+
+
+let processCommand (state: State) (command: Command) =
     try
         match command with
-        | GitStatus -> ()
+        | GitStatus -> Bash "git status" |> printf "%s"
     with e -> printfn "Exception: %s" e.Message
---
+++ Speech/shellspeak.sh
@@ -0,0 +1,1 @@
+./ShellSpeak/bin/Debug/net48/ShellSpeak.exe 0</dev/null &
